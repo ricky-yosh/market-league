8b65c3bb6d363e3e14974a56e0ea6771b8777e5d
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2024-10-09 17:03:32 -0500

    Add environment variables

--- market-league-front-end/src/app/app.component.ts
+++ market-league-front-end/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestButtonComponent } from "./test-button/test-button.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestButtonComponent],
+  imports: [RouterOutlet, TestButtonComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
