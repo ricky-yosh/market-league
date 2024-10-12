d67b4a753daf58708deb5eed6f7d3d1f30bb6fa3
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2024-10-12 15:04:31 -0500

    Create new components with correct color

--- market-league-front-end/src/app/home/home.component.ts
+++ market-league-front-end/src/app/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  imports: [FooterComponent, NavbarComponent]
 })
 export class HomeComponent {
 
