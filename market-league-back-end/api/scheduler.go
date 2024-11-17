243d9d06ff7ecb5c6fefc966d3260f1ce9e007ba
TimotheePommier <timothee.pommier7@gmail.com>
2024-11-17 04:15:24 -0600

    Add change price not finish

--- market-league-back-end/api/scheduler.go
+++ market-league-back-end/api/scheduler.go
@@ -35,6 +35,8 @@ func StartDailyTask() {
                     log.Printf("Error fetching stock data: %v", err)
                 } else {
                     log.Printf("Fetched stock data: %+v", quote)
+                    // NEED TO OPEN STOCK INFO
+                    UpdateStockCurrentPrice(company,quote)
                 }
                 time.Sleep(40 * time.Millisecond)
             }
@@ -45,4 +47,15 @@ func StartDailyTask() {
         }
     }()
 }
- 
+func (r *scheduler) changeCurrentPrice(symbol string, price float64) {
+    company, err := FindCompany(symbol)
+    return r.db.Model(&models.User{}).Where("TickerSymbol = ?", symbol).Update("CurrentPrice", price).Error
+}
+func (r *scheduler) FindCompany(symbol string) {
+    var foundCompany models.Stock
+    err := r.db.Where("TickerSymbol = ?", symbol).First(&foundCompany).Error
+	if err != nil {
+		return nil, err
+	}
+	return &foundCompany, nil
+}
