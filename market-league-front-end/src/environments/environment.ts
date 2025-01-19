54d4a0909d117ed19840f9977a67443c1a2ac99a
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-01-18 23:43:54 -0600

    Create ssl to attempt https and wss

--- market-league-front-end/src/environments/environment.ts
+++ market-league-front-end/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     enableDebug: false,
-    api_url: "http://137.184.77.183",
-    websocket_url: "ws://137.184.77.183"
+    api_url: "https://137.184.77.183",
+    websocket_url: "wss://137.184.77.183"
 };
