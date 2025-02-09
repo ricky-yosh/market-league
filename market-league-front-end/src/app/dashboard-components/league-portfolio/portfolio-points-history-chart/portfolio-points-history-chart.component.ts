ff8ece9bd80ee60f6deb7b98dcbf7084d0031ce2
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-02-08 22:01:19 -0600

    Create charts for the portfolio points history

---
+++ market-league-front-end/src/app/dashboard-components/league-portfolio/portfolio-points-history-chart/portfolio-points-history-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-points-history-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-points-history-chart.component.html',
+  styleUrl: './portfolio-points-history-chart.component.scss'
+})
+export class PortfolioPointsHistoryChartComponent {
+
+}
